e805515335d966d604afe06045c91452cd8e8478
IvanShalimov <gogi-mogel@mail.ru>
2018-04-23 10:22:24 +0300

    fix crash(not implement view state)

--- app/src/main/java/com/example/ivan/simplinictest/mvp/ListViewState.kt
+++ app/src/main/java/com/example/ivan/simplinictest/mvp/ListViewState.kt
@@ -5,15 +5,41 @@ import com.example.ivan.simplinictest.mvp.gui.ListView
 import com.hannesdorfmann.mosby3.mvp.viewstate.RestorableViewState
 
 class ListViewState: RestorableViewState<ListView> {
-    override fun saveInstanceState(out: Bundle) {
 
+    companion object {
+        val LOADING = 0
+        val SHOW_LIST_CITIES = 1
+        val SHOW_LIST_HOSTEL = 2
+
+        //key for save
+        val STATE = "state"
+    }
+
+    var currentViewState = 0
+
+    override fun saveInstanceState(out: Bundle) {
+        out.putInt(STATE,currentViewState)
     }
 
     override fun apply(view: ListView?, retained: Boolean) {
 
+        when(currentViewState){
+            LOADING ->{
+
+            }
+            SHOW_LIST_CITIES ->{
+
+            }
+            SHOW_LIST_HOSTEL ->{
+
+            }
+        }
     }
 
     override fun restoreInstanceState(`in`: Bundle?): RestorableViewState<ListView> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if(`in`!= null){
+            currentViewState = `in`.getInt(STATE)
+        }
+        return this
     }
 }
